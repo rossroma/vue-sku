d4811ef054078c343b6aacd15d6ac3414ee74896
杨凯 <906580223@qq.com>
2018-09-29 20:49:47 +0800

    feat: 实战demo和数据格式

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <!-- 高级规格 -->
+    <!-- 规格设置 -->
     <div class="specification">
       <div class="title">产品规格设置</div>
       <ul class="spec-list">
@@ -26,21 +26,20 @@
       </div>
     </div>
 
-    <!-- 样式一 -->
+    <!-- 规格展示 -->
     <div class="example">
-      <h4 class="title">产品规格样式一</h4>
+      <h4 class="title">规格展示</h4>
       <table class="stock-table">
         <thead>
           <tr>
-            <th v-for="item in specification">{{item.name}}</th>
+            <th v-for="(item, index) in specification" :key="index">{{item.name}}</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="(item, index) in countSum(0)" :key="index">
             <td
               v-for="(n, specIndex) in specification.length"
-              v-if="showTd(specIndex, index)"
-              :rowspan="countSum(n)">
+              :key="n">
               {{getSpecAttr(specIndex, index)}}
             </td>
           </tr>
@@ -48,44 +47,213 @@
       </table>
     </div>
 
-    <!-- 样式二 -->
+    <!-- 实战DEMO -->
     <div class="example">
-      <h4 class="title">产品规格样式二</h4>
-      <table class="stock-table">
+      <h4 class="title">实战DEMO</h4>
+      <table class="stock-table" cellspacing="0" cellpadding="0">
         <thead>
           <tr>
-            <th v-for="item in specification">{{item.name}}</th>
+            <th
+            v-for="(item, index) in specification"
+            :key="index">
+            {{item.name}}
+            </th>
+            <th style="width: 160px;">规格编码</th>
+            <th>成本价（元）</th>
+            <th>库存</th>
+            <th>销售价（元）</th>
+            <th>是否启用</th>
           </tr>
         </thead>
-        <tbody>
-          <tr v-for="(item, index) in countSum(0)" :key="index">
+        <tbody v-if="specification[0] && specification[0].value.length">
+          <tr
+            :key="index"
+            v-for="(item, index) in countSum(0)">
             <td
-              v-for="(n, specIndex) in specification.length">
-              {{getSpecAttr2(specIndex, index)}}
+              v-for="(n, specIndex) in specification.length"
+              v-if="showTd(specIndex, index)"
+              :key="n"
+              :rowspan="countSum(n)">
+              {{getSpecAttr(specIndex, index)}}
+            </td>
+            <td>
+              <el-input
+                size="small"
+                type="text"
+                :disabled="!childProductArray[index].isUse"
+                v-model="childProductArray[index].childProductNo"
+                @blur="handleNo(index)"
+                placeholder="输入商品规格编号">
+              </el-input>
+            </td>
+            <td>
+              <el-input
+                size="small"
+                type="text"
+                v-model.number="childProductArray[index].childProductCost"
+                placeholder="输入成本价"
+                :disabled="!childProductArray[index].isUse">
+              </el-input>
+            </td>
+            <td>
+              <el-input
+                size="small"
+                type="text"
+                v-model.number="childProductArray[index].childProductStock"
+                placeholder="输入库存"
+                :disabled="!childProductArray[index].isUse">
+              </el-input>
+            </td>
+            <td>
+              <el-input
+              size="small"
+              type="text"
+              v-model.number="childProductArray[index].childProductPrice"
+              placeholder="输入销售价"
+              :disabled="!childProductArray[index].isUse">
+            </el-input>
+            </td>
+            <td>
+              <el-switch v-model="childProductArray[index].isUse" @change="(val) => {handleUserChange(index, val)}"></el-switch>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="8" class="wh-foot">
+              <span class="label">批量设置：</span>
+              <ul class="set-list" v-if="isSetListShow">
+                <li class="set-item" @click="openBatch('childProductCost')">成本价</li>
+                <li class="set-item" @click="openBatch('childProductStock')">库存</li>
+                <li class="set-item" @click="openBatch('childProductPrice')">销售价</li>
+              </ul>
+              <div class="set-form" v-else>
+                <el-input size="mini" v-model.number="batchValue" placeholder="输入要设置的数量"></el-input>
+                <i class="set-btn blue el-icon-check" @click="setBatch"></i>
+                <i class="set-btn red el-icon-close" @click="cancelBatch"></i>
+              </div>
             </td>
           </tr>
         </tbody>
+
       </table>
     </div>
 
-
+    <div class="example">
+      <h4 class="title">数据格式</h4>
+      <textarea class="code-area" :value="JSON.stringify(childProductArray)"></textarea>
+    </div>
   </div>
 </template>
 
 <script>
+
+// 为Object添加一个原型方法，判断两个对象是否相等
+function objEquals (object1, object2) {
+  // For the first loop, we only check for types
+  for (let propName in object1) {
+    // Check for inherited methods and properties - like .equals itself
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/hasOwnProperty
+    // Return false if the return value is different
+    if (object1.hasOwnProperty(propName) !== object2.hasOwnProperty(propName)) {
+      return false
+    // Check instance type
+    } else if (typeof object1[propName] !== typeof object2[propName]) {
+      // Different types => not equal
+      return false
+    }
+  }
+  // Now a deeper check using other objects property names
+  for (let propName in object2) {
+    // We must check instances anyway, there may be a property that only exists in object2
+    // I wonder, if remembering the checked values from the first loop would be faster or not
+    if (object1.hasOwnProperty(propName) !== object2.hasOwnProperty(propName)) {
+      return false
+    } else if (typeof object1[propName] !== typeof object2[propName]) {
+      return false
+    }
+    // If the property is inherited, do not check any more (it must be equa if both objects inherit it)
+    if (!object1.hasOwnProperty(propName)) {
+      continue
+    }
+    // Now the detail check and recursion
+    // This returns the script back to the array comparing
+    /** REQUIRES Array.equals**/
+    if (object1[propName] instanceof Array && object2[propName] instanceof Array) {
+      // recurse into the nested arrays
+      if (objEquals(!object1[propName], object2[propName])) {
+        return false
+      }
+    } else if (object1[propName] instanceof Object && object2[propName] instanceof Object) {
+      // recurse into another objects
+      // console.log("Recursing to compare ", this[propName],"with",object2[propName], " both named \""+propName+"\"");
+      if (objEquals(!object1[propName], object2[propName])) {
+        return false
+      }
+    // Normal value comparison for strings and numbers
+    } else if (object1[propName] !== object2[propName]) {
+      return false
+    }
+  }
+  // If everything passed, let's say YES
+  return true
+}
 
 export default {
   name: 'app',
 
   data () {
     return {
+      // 规格
       specification: [
         {
           name: '颜色',
-          value: ['黑色', '红色']
+          value: ['黑色', '白色', '蓝色']
         }
       ],
-      addValues: [] // 用来存储要添加的规格属性
+      // 子规格
+      childProductArray: [
+        {
+          childProductId: 0,
+          childProductSpec: {'颜色': '黑色'},
+          childProductNo: 'PRODUCTNO_0',
+          childProductStock: 0,
+          childProductPrice: 0,
+          childProductCost: 0,
+          isUse: true
+        },
+        {
+          childProductId: 0,
+          childProductSpec: {'颜色': '白色'},
+          childProductNo: 'PRODUCTNO_1',
+          childProductStock: 0,
+          childProductPrice: 0,
+          childProductCost: 0,
+          isUse: true
+        },
+        {
+          childProductId: 0,
+          childProductSpec: {'颜色': '蓝色'},
+          childProductNo: 'PRODUCTNO_2',
+          childProductStock: 0,
+          childProductPrice: 0,
+          childProductCost: 0,
+          isUse: true
+        }
+      ],
+      // 用来存储要添加的规格属性
+      addValues: [],
+      // 默认商品编号
+      defaultProductNo: 'PRODUCTNO_',
+      // 批量设置相关
+      isSetListShow: true,
+      batchValue: '', // 批量设置所绑定的值
+      currentType: '' // 要批量设置的类型
+    }
+  },
+
+  computed: {
+    // 所有sku的id
+    stockSpecArr () {
+      return this.childProductArray.map(item => item.childProductSpec)
     }
   },
 
@@ -103,21 +271,31 @@ export default {
     // 删除规格项目
     delSpec (index) {
       this.specification.splice(index, 1)
+
+      this.handleSpecChange('del')
     },
 
     // 添加规格属性
     addSpecTag (index) {
-      let arr = this.addValues[index].trim().split(' ')
+      let str = this.addValues[index] || ''
+      if (!str.trim()) return // 判空
+      str = str.trim()
+      let arr = str.split(/\s+/) // 使用空格分割成数组
+
       let newArr = this.specification[index].value.concat(arr)
       newArr = Array.from(new Set(newArr)) // 去重
       this.$set(this.specification[index], 'value', newArr)
 
       this.clearAddValues(index)
+
+      this.handleSpecChange('add')
     },
 
     // 删除规格属性
     delSpecTag (index, num) {
       this.specification[index].value.splice(num, 1)
+
+      this.handleSpecChange('del')
     },
 
     // 清空 addValues
@@ -132,26 +310,6 @@ export default {
         index 所有属性在遍历时的序号
     */
     getSpecAttr (specIndex, index) {
-      // 获取当前规格项目下的属性值
-      const currentValues = this.specification[specIndex].value
-      let indexCopy
-
-      // 判断是否是最后一个规格项目
-      if (this.specification[specIndex + 1] && this.specification[specIndex + 1].value.length) {
-        indexCopy = index / this.countSum(specIndex + 1)
-      } else {
-        indexCopy = index
-      }
-
-      const i = indexCopy % currentValues.length
-      if (i.toString() !== 'NaN' && i % 1 === 0) {
-        return currentValues[i]
-      } else {
-        return ''
-      }
-    },
-
-    getSpecAttr2 (specIndex, index) {
       // 获取当前规格项目下的属性值
       const currentValues = this.specification[specIndex].value
       let indexCopy
@@ -199,6 +357,151 @@ export default {
       } else {
         return false
       }
+    },
+
+    /**
+    * [handleSpecChange 监听标签的变化,当添加标签时；求出每一行的hash id，再动态变更库存信息；当删除标签时，先清空已有库存信息，然后将原有库存信息暂存到stockCopy中，方便后面调用]
+    * @param  {[string]} option ['add'|'del' 操作类型，添加或删除]
+    * @return {[type]}        [description]
+    */
+    handleSpecChange (option) {
+      const stockCopy = JSON.parse(JSON.stringify(this.childProductArray))
+      if (option === 'del') {
+        this.childProductArray = []
+      }
+
+      for (let i = 0; i < this.countSum(0); i++) {
+        this.changeStock(option, i, stockCopy)
+      }
+    },
+
+    /**
+     * [根据规格，动态改变库存相关信息]
+     * @param  {[string]} option    ['add'|'del' 操作类型，添加或删除]
+     * @param  {[array]} stockCopy [库存信息的拷贝]
+     * @return {[type]}           [description]
+     */
+    changeStock (option, index, stockCopy) {
+      let childProduct = {
+        childProductId: 0,
+        childProductSpec: this.getChildProductSpec(index),
+        childProductNo: this.defaultProductNo + index,
+        childProductStock: 0,
+        childProductPrice: 0,
+        childProductCost: 0,
+        isUse: true
+      }
+
+      const spec = childProduct.childProductSpec
+      if (option === 'add') {
+        // 如果此id不存在，说明为新增属性，则向 childProductArray 中添加一条数据
+        if (this.stockSpecArr.findIndex((item) => objEquals(spec, item)) === -1) {
+          this.$set(this.childProductArray, index, childProduct)
+        }
+      } else if (option === 'del') {
+        // 因为是删除操作，理论上所有数据都能从stockCopy中获取到
+        let origin = ''
+        stockCopy.forEach(item => {
+          if (objEquals(spec, item.childProductSpec)) {
+            origin = item
+            return false
+          }
+        })
+        this.childProductArray.push(origin || childProduct)
+      }
+    },
+
+    // 获取childProductArray的childProductSpec属性
+    getChildProductSpec (index) {
+      let obj = {}
+      this.specification.forEach((item, specIndex) => {
+        obj[item.name] = this.getSpecAttr(specIndex, index)
+      })
+      return obj
+    },
+
+    // 监听规格启用操作
+    handleUserChange (index, value) {
+      // 启用规格时，生成不重复的商品编号；关闭规格时，清空商品编号
+      if (value) {
+        let No = this.makeProductNoNotRepet(index)
+        this.$set(this.childProductArray[index], 'childProductNo', No)
+      } else {
+        this.$set(this.childProductArray[index], 'childProductNo', '')
+      }
+    },
+
+    // 监听商品编号的blur事件
+    handleNo (index, attr) {
+      // 1.当用户输入完商品编号时，判断是否重复，如有重复，则提示客户并自动修改为不重复的商品编号
+      const value = this.childProductArray[index].childProductNo
+      let isRepet
+
+      this.childProductArray.forEach((item, i) => {
+        if (item.childProductNo === value && i !== index) {
+          isRepet = true
+        }
+      })
+
+      if (isRepet) {
+        this.$message({
+          type: 'warning',
+          message: '不允许输入重复的商品编号'
+        })
+        this.$set(this.childProductArray[index], 'childProductNo', this.makeProductNoNotRepet(index))
+      }
+    },
+
+    // 生成不重复的商品编号
+    makeProductNoNotRepet (index) {
+      let No
+      let i = index
+      let isRepet = true
+      while (isRepet) {
+        No = this.defaultProductNo + i
+        isRepet = this.isProductNoRepet(No)
+        i++
+      }
+      return No
+    },
+
+    // 商品编号判重
+    isProductNoRepet (No) {
+      const result = this.childProductArray.findIndex((item) => {
+        return item.childProductNo === No
+      })
+      return result > -1
+    },
+
+    // 打开设置框
+    openBatch (type) {
+      this.currentType = type
+      this.isSetListShow = false
+    },
+
+    // 批量设置
+    setBatch () {
+      if (typeof this.batchValue === 'string') {
+        this.$message({
+          type: 'warning',
+          message: '请输入正确的值'
+        })
+        return
+      }
+      this.childProductArray.forEach(item => {
+        if (item.isUse) {
+          item[this.currentType] = this.batchValue
+        }
+      })
+
+      this.cancelBatch()
+    },
+
+    // 取消批量设置
+    cancelBatch () {
+      this.batchValue = ''
+      this.currentType = ''
+      this.isSetListShow = true
     }
   }
 }
@@ -223,6 +526,15 @@ export default {
   }
   .example {
     margin-top: 50px;
+    .code-area {
+      width: 740px;
+      height: 300px;
+      box-sizing: border-box;
+      padding: 20px;
+      border: 2px dashed #c00;
+      font-size: 14px;
+      line-height: 1.6;
+    }
   }
   .specification {
     display: inline-block;
@@ -267,6 +579,7 @@ export default {
           .add-attr {
             display: inline-block;
             vertical-align: top;
+            margin-top: 4px;
             .el-input {
               width: 200px;
               margin: 2px 0 0 4px;
@@ -280,12 +593,74 @@ export default {
     }
   }
   .stock-table {
+    width: 740px;
+    padding: 0;
+    border-collapse: separate;
+    border-color: #dfe6ec;
+    border-style: solid;
+    border-width: 1px 0 0 1px;
+    background-color: #fff;
     td,
     th {
-      padding: 5px 10px;
-      border: 1px solid #ddd;
-      width: 100px;
-      font-size: 14px;
+      padding: 4px 10px;
+      border-bottom: 1px solid #dfe6ec;
+      border-right: 1px solid #dfe6ec;
+    }
+    th {
+      line-height: 30px;
+      background-color: #eef1f6;
+    }
+    .link {
+      cursor: pointer;
+      color: #0088ee;
+      font-size: 13px;
+      margin-left: 6px;
+    }
+    .wh-foot {
+      line-height: 30px;
+      .label {
+        display: inline-block;
+        vertical-align: top;
+      }
+      .set-list {
+        display: inline-block;
+        vertical-align: top;
+        font-size: 0;
+        .set-item {
+          display: inline-block;
+          vertical-align: top;
+          margin-left: 15px;
+          font-size: 13px;
+          cursor: pointer;
+          color: #0088ee;
+        }
+      }
+      .set-form {
+        display: inline-block;
+        margin-left: 10px;
+        .el-input {
+          display: inline-block;
+          width: 120px;
+        }
+        .set-btn {
+          display: inline-block;
+          padding: 0 2px;
+          font-size: 15px;
+          cursor: pointer;
+          &.blue {
+            color: #0088ee;
+          }
+          &.red {
+            color: #cc0000;
+          }
+        }
+      }
+      .right {
+        float: right;
+      }
+      .text {
+        font-size: 13px;
+      }
     }
   }
 }
