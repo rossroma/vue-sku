bc62ec70c015cf417c01f157d374243e56d22ba1
blackcai <896339934@qq.com>
2019-11-08 17:04:23 +0800

    修改说明文档

---
+++ README_en.md
@@ -0,0 +1,31 @@
+# vue-sku
+Base on vuejs，a product sku component.
+
+[version 1.0](http://www.opnnn.com/sku2?version=git)
+
+[version 1.1](http://www.opnnn.com/sku2/index2?version=git)
+
+# start
+```
+  npm install
+  npm run serve
+```
+visit：[http://localhost:8080](http://www.opnnn.com/sku2/index2?version=git)
+
+##### Update
+* Add English DOC
+
+##### Update 1.1.1
+
+* Add route
+
+* Add version preview
+
+* Create component
+
+##### Version 1.1
+* Add specification name validation duplicate and null
+
+* Update UI
+
+#####  Initialise version。
--- src/components/sku.v1.1.zh.vue
+++ src/components/sku.v1.1.zh.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="vue-sku">
-    <!-- specification setting -->
+    <!-- 规格设置 -->
     <el-card class="box-card">
       <div slot="header" class="clearfix">
-        <span>Product Specification Setting</span>
+        <span>产品规格设置</span>
       </div>
       <section>
         <div v-for="(item, index) in specification" :key="index" class="spec-line">
           <div>
             <span v-if="!cacheSpecification[index].status">{{ item.name }}</span>
-            <el-input size="small" style="width:200px;" v-if="cacheSpecification[index].status" v-model="cacheSpecification[index].name" placeholder="Product Specifications" @keyup.native.enter="saveSpec(index)">
+            <el-input size="small" style="width:200px;" v-if="cacheSpecification[index].status" v-model="cacheSpecification[index].name" placeholder="输入产品规格" @keyup.native.enter="saveSpec(index)">
               <el-button slot="append" icon="el-icon-check" type="primary" @click="saveSpec(index)"></el-button>
             </el-input>
             <i class="icon el-icon-edit" v-if="!cacheSpecification[index].status" @click="updateSpec(index)"></i>
           </div>
           <div>
             <el-tag v-for="(tag, j) in item.value" :key="j" closable  @close="delSpecTag(index, j)">{{ tag }}</el-tag>
-            <el-input size="small" style="width:200px;" v-model="addValues[index]" placeholder="Multiple Product Properties Are Separated By Spaces" @keyup.native.enter="addSpecTag(index)">
+            <el-input size="small" style="width:200px;" v-model="addValues[index]" placeholder="多个产品属性以空格隔开" @keyup.native.enter="addSpecTag(index)">
               <el-button slot="append" icon="el-icon-check" type="primary" @click="addSpecTag(index)"></el-button>
             </el-input>
           </div>
@@ -24,16 +24,16 @@
           <el-divider></el-divider>
         </div>
         <div class="add-spec">
-          <el-button size="small" type="primary" :disabled="specification.length >= 5" @click="addSpec">Add New Specification</el-button>
+          <el-button size="small" type="primary" :disabled="specification.length >= 5" @click="addSpec">添加规格项目</el-button>
         </div>
       </section>
     </el-card>
 
-    <!-- specification display -->
+    <!-- 规格展示 -->
     <el-card class="box-card">
       <div slot="header" class="clearfix">
-        <span>Specification Display</span>
-        <el-button style="float: right; padding: 3px 0" type="text" @click="specificationStatus = !specificationStatus">{{ !specificationStatus ? 'show' : 'hide' }}</el-button>
+        <span>规格展示</span>
+        <el-button style="float: right; padding: 3px 0" type="text" @click="specificationStatus = !specificationStatus">{{ !specificationStatus ? '显示' : '隐藏' }}</el-button>
       </div>
       <section v-if="specificationStatus">
         <el-row>
@@ -45,10 +45,10 @@
       </section>
     </el-card>
 
-    <!-- DEMO -->
+    <!-- 实战DEMO -->
     <el-card>
       <div slot="header" class="clearfix">
-        <span>Specification Table</span>
+        <span>规格表格</span>
       </div>
       <table class="el-table" cellspacing="0" cellpadding="0">
         <thead>
@@ -58,11 +58,11 @@
             :key="index">
             {{item.name}}
           </th>
-          <th style="width: 160px;">Specification Number</th>
-          <th>Cost Price (Yuan)</th>
-          <th>Stock</th>
-          <th>Sale Price（Yuan）</th>
-          <th>Use</th>
+          <th style="width: 160px;">规格编码</th>
+          <th>成本价（元）</th>
+          <th>库存</th>
+          <th>销售价（元）</th>
+          <th>是否启用</th>
         </tr>
         </thead>
         <tbody v-if="specification[0] && specification[0].value.length">
@@ -84,7 +84,7 @@
               :disabled="!childProductArray[index].isUse"
               v-model="childProductArray[index].childProductNo"
               @blur="handleNo(index)"
-              placeholder="Product Specifications Number">
+              placeholder="输入商品规格编号">
             </el-input>
           </td>
           <td>
@@ -92,7 +92,7 @@
               size="small"
               type="text"
               v-model.number="childProductArray[index].childProductCost"
-              placeholder="Cost Price"
+              placeholder="输入成本价"
               :disabled="!childProductArray[index].isUse">
             </el-input>
           </td>
@@ -101,7 +101,7 @@
               size="small"
               type="text"
               v-model.number="childProductArray[index].childProductStock"
-              placeholder="Stock"
+              placeholder="输入库存"
               :disabled="!childProductArray[index].isUse">
             </el-input>
           </td>
@@ -110,7 +110,7 @@
               size="small"
               type="text"
               v-model.number="childProductArray[index].childProductPrice"
-              placeholder="Sale Price"
+              placeholder="输入销售价"
               :disabled="!childProductArray[index].isUse">
             </el-input>
           </td>
@@ -120,14 +120,14 @@
         </tr>
         <tr>
           <td colspan="8" class="wh-foot">
-            <span class="label">Batch Settings：</span>
+            <span class="label">批量设置：</span>
             <template v-if="isSetListShow">
-              <el-button @click="openBatch('childProductCost')" size="mini">Cost Price</el-button>
-              <el-button @click="openBatch('childProductStock')" size="mini">Stock</el-button>
-              <el-button @click="openBatch('childProductPrice')" size="mini">Sale Price</el-button>
+              <el-button @click="openBatch('childProductCost')" size="mini">成本价</el-button>
+              <el-button @click="openBatch('childProductStock')" size="mini">库存</el-button>
+              <el-button @click="openBatch('childProductPrice')" size="mini">销售价</el-button>
             </template>
             <template v-else>
-              <el-input size="mini" style="width:200px;" v-model.number="batchValue" placeholder="Setting Price"></el-input>
+              <el-input size="mini" style="width:200px;" v-model.number="batchValue" placeholder="输入要设置的数量"></el-input>
               <el-button type="primary" size="mini" @click="setBatch"><i class="set-btn blue el-icon-check"></i></el-button>
               <el-button type="danger" size="mini" @click="cancelBatch"><i class="set-btn blue el-icon-close"></i></el-button>
             </template>
@@ -138,10 +138,10 @@
       </table>
     </el-card>
 
-    <!-- Data Form -->
+    <!-- 数据格式 -->
     <el-card class="box-card">
       <div slot="header" class="clearfix">
-        <span>Data Form</span>
+        <span>数据格式</span>
       </div>
       <section>
         <div v-for="(item, index) in childProductArray" :key="index">
@@ -155,7 +155,7 @@
 
 <script>
 
-  // Add a prototype method for Object，It is determined whether or not the two objects are equal.
+  // 为Object添加一个原型方法，判断两个对象是否相等
   function objEquals (object1, object2) {
     // For the first loop, we only check for types
     for (let propName in object1) {
@@ -211,25 +211,25 @@
 
     data () {
       return {
-        specificationStatus: false, // Show specification list
-        // Specification
+        specificationStatus: false, // 显示规格列表
+        // 规格
         specification: [],
-        // Child specification
+        // 子规格
         childProductArray: [],
-        // Used to store specification properties to be added
+        // 用来存储要添加的规格属性
         addValues: [],
-        // Default product number
+        // 默认商品编号
         defaultProductNo: 'PRODUCTNO_',
-        // Batch setting status
+        // 批量设置相关
         isSetListShow: true,
-        batchValue: '', // Batch sets the value of the binding
-        currentType: '', // Batch sets the type of the binding
-        cacheSpecification: [] // Cache specification
+        batchValue: '', // 批量设置所绑定的值
+        currentType: '', // 要批量设置的类型
+        cacheSpecification: [] // 缓存规格名称
       }
     },
 
     computed: {
-      // All sku's id
+      // 所有sku的id
       stockSpecArr () {
         return this.childProductArray.map(item => item.childProductSpec)
       }
@@ -238,24 +238,24 @@
       this.createData()
     },
     methods: {
-      // Analog data
+      // 创建模拟数据
       createData() {
-        const arr = ['Color', 'Size']
-        const arr2 = ['Black White Blue', 's m xl']
+        const arr = ['颜色', '尺寸']
+        const arr2 = ['黑色 白色 蓝色', 's m xl']
         for (let i = 0; i < 2; i++) {
-          // Add data
+          // 添加数据
           this.addSpec()
-          // Data
+          // 数据
           this.specification[i].name = arr[i]
           this.addValues[i] = arr2[i]
-          // Cache button value
+          // 缓存按钮键值
           this.cacheSpecification[i].status = false
           this.cacheSpecification[i].name = arr[i]
-          // Generate
+          // 构建
           this.addSpecTag(i)
         }
       },
-      // Add specification items
+      // 添加规格项目
       addSpec () {
         if (this.specification.length < 5) {
           this.cacheSpecification.push({
@@ -268,32 +268,32 @@
           })
         }
       },
-      // Update status
+      // 修改状态
       updateSpec(index) {
         this.cacheSpecification[index].status = true
         this.cacheSpecification[index].name = this.specification[index].name
       },
-      // Save specification name
+      // 保存规格名
       saveSpec(index) {
         if (!this.cacheSpecification[index].name.trim()) {
-          this.$message.error('The name cannot be empty')
+          this.$message.error('名称不能为空，请注意修改')
           return
         }
-        // Save needs to verify that the name is duplicated
+        // 保存需要验证名称是否重复
         if (this.specification[index].name === this.cacheSpecification[index].name) {
           this.cacheSpecification[index].status = false
         } else {
 
           if (this.verifyRepeat(index)) {
-            // If there is repetition, can't be modified
-            this.$message.error('Duplicate name')
+            // 如果有重复的，禁止修改
+            this.$message.error('名称重复，请注意修改')
           } else {
             this.specification[index].name = this.cacheSpecification[index].name
             this.cacheSpecification[index].status = false
           }
         }
       },
-      // delete specification item
+      // 删除规格项目
       delSpec (index) {
         this.specification.splice(index, 1)
 
@@ -302,7 +302,7 @@
       verifyRepeat(index) {
         let flag = false
         this.specification.forEach((value, j) => {
-          // Check for duplicate,if true, can't modify
+          // 检查除了当前选项以外的值是否和新的值想等，如果相等，则禁止修改
           if (index !== j) {
             if (this.specification[j].name === this.cacheSpecification[index].name) {
               flag = true
@@ -311,19 +311,18 @@
         })
         return flag
       },
-      // Add specification
+      // 添加规格属性
       addSpecTag (index) {
         let str = this.addValues[index] || ''
-        // Check empty
         if (!str.trim() || !this.cacheSpecification[index].name.trim()) {
-          this.$message.error("specification can't be empty")
+          this.$message.error('名称不能为空，请注意修改')
           return
-        }
+        } // 判空
         str = str.trim()
-        let arr = str.split(/\s+/) // Split into arrays using spaces
+        let arr = str.split(/\s+/) // 使用空格分割成数组
 
         let newArr = this.specification[index].value.concat(arr)
-        newArr = Array.from(new Set(newArr)) // de-duplication
+        newArr = Array.from(new Set(newArr)) // 去重
         this.$set(this.specification[index], 'value', newArr)
 
         this.clearAddValues(index)
@@ -333,30 +332,30 @@
         this.cacheSpecification[index].status = false
       },
 
-      // Delete specification
+      // 删除规格属性
       delSpecTag (index, num) {
         this.specification[index].value.splice(num, 1)
 
         this.handleSpecChange('del')
       },
 
-      // clear addValues
+      // 清空 addValues
       clearAddValues (index) {
         this.$set(this.addValues, index, '')
       },
 
       /*
-        According to the value of the property passed in, get the property of the corresponding specification.
+        根据传入的属性值，拿到相应规格的属性
         @params
-          specIndex: Serial number of specification item in advancedSpecification
-          index: The ordinal number when the property is traversing
+          specIndex 规格项目在 advancedSpecification 中的序号
+          index 所有属性在遍历时的序号
       */
       getSpecAttr (specIndex, index) {
-        // Gets the property value of the current specification item
+        // 获取当前规格项目下的属性值
         const currentValues = this.specification[specIndex].value
         let indexCopy
 
-        // Judge the last specification item
+        // 判断是否是最后一个规格项目
         if (this.specification[specIndex + 1] && this.specification[specIndex + 1].value.length) {
           indexCopy = index / this.countSum(specIndex + 1)
         } else {
@@ -373,9 +372,9 @@
       },
 
       /*
-        The product of the calculated properties
+        计算属性的乘积
         @params
-          specIndex: Serial number of specification item in advancedSpecification
+          specIndex 规格项目在 advancedSpecification 中的序号
       */
       countSum (specIndex) {
         let num = 1
@@ -387,13 +386,13 @@
         return num
       },
 
-      // Based on the incoming criteria, determine whether to display the td
+      // 根据传入的条件，来判断是否显示该td
       showTd (specIndex, index) {
-        // If the current item does not have any property, it does not display
+        // 如果当前项目下没有属性，则不显示
         if (!this.specification[specIndex]) {
           return false
 
-          // Understand by yourself
+          // 自己悟一下吧
         } else if (index % this.countSum(specIndex + 1) === 0) {
           return true
         } else {
@@ -402,8 +401,8 @@
       },
 
       /**
-       * [handleSpecChange: When the label is added, the change of the label is monitored; the hash id of each line is obtained and the inventory information is dynamically changed; when the label is deleted, the existing inventory information is first emptied, the original inventory information is temporarily stored in the stockCopy, and the later call is convenient]
-       * @param  {[string]} option ['add'|'del' Operation type，add or deleted]
+       * [handleSpecChange 监听标签的变化,当添加标签时；求出每一行的hash id，再动态变更库存信息；当删除标签时，先清空已有库存信息，然后将原有库存信息暂存到stockCopy中，方便后面调用]
+       * @param  {[string]} option ['add'|'del' 操作类型，添加或删除]
        * @return {[type]}        [description]
        */
       handleSpecChange (option) {
@@ -418,9 +417,9 @@
       },
 
       /**
-       * [Dynamically change inventory-related information according to specifications.]
-       * @param  {[string]} option    ['add'|'del', Operation type，add or deleted]
-       * @param  {[array]} stockCopy [Copy of inventory information]
+       * [根据规格，动态改变库存相关信息]
+       * @param  {[string]} option    ['add'|'del' 操作类型，添加或删除]
+       * @param  {[array]} stockCopy [库存信息的拷贝]
        * @return {[type]}           [description]
        */
       changeStock (option, index, stockCopy) {
@@ -436,12 +435,12 @@
 
         const spec = childProduct.childProductSpec
         if (option === 'add') {
-          // If the id does not exist, the description is a new attribute, and a data is added to the child Array
+          // 如果此id不存在，说明为新增属性，则向 childProductArray 中添加一条数据
           if (this.stockSpecArr.findIndex((item) => objEquals(spec, item)) === -1) {
             this.$set(this.childProductArray, index, childProduct)
           }
         } else if (option === 'del') {
-          // Because it's a delete operation, in theory, all the data can be obtained from stockCopy.
+          // 因为是删除操作，理论上所有数据都能从stockCopy中获取到
           let origin = ''
           stockCopy.forEach(item => {
             if (objEquals(spec, item.childProductSpec)) {
@@ -453,7 +452,7 @@
         }
       },
 
-      // Gets the childProductSpec property of the childProductArray.
+      // 获取childProductArray的childProductSpec属性
       getChildProductSpec (index) {
         let obj = {}
         this.specification.forEach((item, specIndex) => {
@@ -462,9 +461,9 @@
         return obj
       },
 
-      // Monitoring specification enable operation
+      // 监听规格启用操作
       handleUserChange (index, value) {
-        // Generate a non-duplicate product when the specification is enabled; clear the product number when the specification is turned off.
+        // 启用规格时，生成不重复的商品编号；关闭规格时，清空商品编号
         if (value) {
           let No = this.makeProductNoNotRepet(index)
           this.$set(this.childProductArray[index], 'childProductNo', No)
@@ -473,9 +472,9 @@
         }
       },
 
-      // Listening to the blur event of product number
+      // 监听商品编号的blur事件
       handleNo (index) {
-        /* When the user enters the item number, it will judge whether it is repeated. If there is any repetition, it will prompt the user and automatically modify the item number to not repeat */
+        // 1.当用户输入完商品编号时，判断是否重复，如有重复，则提示客户并自动修改为不重复的商品编号
         const value = this.childProductArray[index].childProductNo
         let isRepet
 
@@ -488,13 +487,13 @@
         if (isRepet) {
           this.$message({
             type: 'warning',
-            message: 'Duplicate item numbers are not allowed to be entered'
+            message: '不允许输入重复的商品编号'
           })
           this.$set(this.childProductArray[index], 'childProductNo', this.makeProductNoNotRepet(index))
         }
       },
 
-      // Generate a non-duplicate product number
+      // 生成不重复的商品编号
       makeProductNoNotRepet (index) {
         let No
         let i = index
@@ -507,7 +506,7 @@
         return No
       },
 
-      // Product number judgment of repetition
+      // 商品编号判重
       isProductNoRepet (No) {
         const result = this.childProductArray.findIndex((item) => {
           return item.childProductNo === No
@@ -515,18 +514,18 @@
         return result > -1
       },
 
-      // Open the setting modal
+      // 打开设置框
       openBatch (type) {
         this.currentType = type
         this.isSetListShow = false
       },
 
-      // Batch setting
+      // 批量设置
       setBatch () {
         if (typeof this.batchValue === 'string') {
           this.$message({
             type: 'warning',
-            message: 'Please enter the correct value'
+            message: '请输入正确的值'
           })
           return
         }
@@ -539,7 +538,7 @@
         this.cancelBatch()
       },
 
-      // Batch setting cancel
+      // 取消批量设置
       cancelBatch () {
         this.batchValue = ''
         this.currentType = ''
@@ -550,12 +549,12 @@
 </script>
 
 <style lang="scss" scoped>
-.vue-sku{
-  .spec-line{position:relative;
-    .spec-deleted{position:absolute;right:0;top:0;display: none;cursor: pointer;}
-    &:hover{
-      .spec-deleted{display: inline;}
+  .vue-sku{
+    .spec-line{position:relative;
+      .spec-deleted{position:absolute;right:0;top:0;display: none;cursor: pointer;}
+      &:hover{
+        .spec-deleted{display: inline;}
+      }
     }
   }
-}
 </style>
